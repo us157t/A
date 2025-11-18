691ee14b5afb99959b61c6a64b53afd3083bb9b8
us157t <us157t@126.com>
2025-11-18 23:02:56 +0800

    Add router hc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::fmt::format;
+
+use axum::{
+	Router, extract::Request, http::StatusCode, response::IntoResponse, routing::get
+};
+
+async fn greet() -> &'static str {
+	"Hello!!!"
+}
+
+async fn hc(req: Request) -> impl IntoResponse {
+	StatusCode::OK
+}
+
+#[tokio::main]
+async fn main() {
+	let app = Router::new()
+			.route("/", get(greet))
+			.route("/hc",get(hc));
+
+	let lis = tokio::net::TcpListener::bind("127.0.0.1:3000")
+			.await
+			.unwrap();
+
+	println!("listening on {}", lis.local_addr().unwrap());
+
+	axum::serve(lis, app).await.unwrap();
 }
